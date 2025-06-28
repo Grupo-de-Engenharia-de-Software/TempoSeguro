cf76078701beac9a39b6fdf32618061fdc382850
Gabriel Perini <gabrielprestesperini@hotmail.com>
2025-06-28 14:32:31 -0300

    - fix logo

--- src/theme/theme-provider.tsx
+++ src/theme/theme-provider.tsx
@@ -1,41 +1,39 @@
-'use client';
+"use client";
 
-import type {} from '@mui/x-tree-view/themeAugmentation';
-import type {} from '@mui/x-data-grid/themeAugmentation';
-import type {} from '@mui/x-date-pickers/themeAugmentation';
-import type {} from '@mui/material/themeCssVarsAugmentation';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider as CssVarsProvider } from "@mui/material/styles";
+import type {} from "@mui/material/themeCssVarsAugmentation";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import type {} from "@mui/x-data-grid/themeAugmentation";
+import type {} from "@mui/x-date-pickers/themeAugmentation";
+import type {} from "@mui/x-tree-view/themeAugmentation";
 
-import CssBaseline from '@mui/material/CssBaseline';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
-
-import { useSettingsContext } from 'src/components/settings';
-
-import { createTheme } from './create-theme';
-import { RTL } from './with-settings/right-to-left';
-import { schemeConfig } from './color-scheme-script';
+import { useSettingsContext } from "src/components/settings";
+import { schemeConfig } from "./color-scheme-script";
+import { createTheme } from "./create-theme";
+import { RTL } from "./with-settings/right-to-left";
 
 // ----------------------------------------------------------------------
 
 type Props = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 };
 
 export function ThemeProvider({ children }: Props) {
-  const settings = useSettingsContext();
+	const settings = useSettingsContext();
 
-  const theme = createTheme(settings);
+	const theme = createTheme(settings);
 
-  return (
-    <AppRouterCacheProvider options={{ key: 'css' }}>
-      <CssVarsProvider
-        theme={theme}
-        defaultMode={schemeConfig.defaultMode}
-        modeStorageKey={schemeConfig.modeStorageKey}
-      >
-        <CssBaseline />
-        <RTL direction={settings.direction}>{children}</RTL>
-      </CssVarsProvider>
-    </AppRouterCacheProvider>
-  );
+	return (
+		<AppRouterCacheProvider options={{ key: "css" }}>
+			<CssVarsProvider
+				theme={theme}
+				defaultMode={schemeConfig.defaultMode}
+				modeStorageKey={schemeConfig.modeStorageKey}
+			>
+				<CssBaseline />
+				<RTL direction={settings.direction}>{children}</RTL>
+			</CssVarsProvider>
+		</AppRouterCacheProvider>
+	);
 }
